83fd34265672ff89a5720c8786e9967adcba496b
Gabi <gabriela.takata@utp.ac.pa>
2025-12-14 17:54:38 -0500

    generador de pdf de vacaciones

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,9 +2,17 @@
 $user = current_user();
 $links = [];
 
-if ($user && in_array($user['rol'], ['administrador', 'recursos_humanos'], true)) {
+if ($user && $user['rol'] === 'administrador') {
     $links[] = ['Gestión de usuarios', BASE_URL . '/index.php?page=gestionar_usuarios'];
     $links[] = ['Cambiar contraseña', BASE_URL . '/index.php?page=cambiar_pw'];
+    $links[] = ['Gestión de colaboradores', BASE_URL . '/index.php?page=gestionar_colaboradores'];
+    $links[] = ['Gestión de cargos', BASE_URL . '/index.php?page=gestionar_cargos'];
+    $links[] = ['Asignar cargo', BASE_URL . '/index.php?page=asignar_cargo'];
+    $links[] = ['Gestionar asistencias', BASE_URL . '/index.php?page=gestionar_asistencias'];
+    $links[] = ['Gestionar vacaciones', BASE_URL . '/index.php?page=gestionar_vacaciones'];
+    $links[] = ['Reporte colaborador/sueldo', BASE_URL . '/index.php?page=reporte_colaborador_sueldo'];
+    $links[] = ['Estadísticas', BASE_URL . '/index.php?page=estadisticas'];
+} elseif ($user && $user['rol'] === 'recursos_humanos') {
     $links[] = ['Gestión de colaboradores', BASE_URL . '/index.php?page=gestionar_colaboradores'];
     $links[] = ['Gestión de cargos', BASE_URL . '/index.php?page=gestionar_cargos'];
     $links[] = ['Asignar cargo', BASE_URL . '/index.php?page=asignar_cargo'];
